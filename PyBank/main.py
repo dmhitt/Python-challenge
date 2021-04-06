834cbf9d037ae5b1903ed4e3bcf7f3bce03209d4
Dinnara Hitt <dinnarahitt@Our-iMac.local>
2021-04-06 14:25:10 -0700

    Changed both main to write the text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,12 @@
+#Name: Dinnara Hitt
+#Python-Challenge
+#PyBank
+
 import os
 import csv
 
 csvpath = os.path.join ('Resources', 'budget_data.csv')
+output_path = os.path.join ("Resources", "output_budget_data.txt")
 
 list_date = []
 list_amount = []
@@ -27,19 +32,22 @@ save_max_previous_amount = 0.00
 increase_amount = 0.00
 
 with open(csvpath) as csvfile:
-	# CSV reader specifies delimiter and variable that holds contents
+	
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read the header row first (skip this step if there is now header)
+    # read the header row first 
     csv_header = next(csvreader)
-    # Read each row of data after the header
     
+    # for loop to read each row of data after the header 
+    # saves the date into list_date and saves amount into list_amount
+        
     for row in csvreader:
     	list_date.append(row[0])
     	list_amount.append(float(row[1]))
     	total_months += 1
     	total_profit_losses += int(row[1])
 
+# for loop that saves the average of two amounts into a list_average 
 for list_index in range(len(list_amount)):
   if first_time:
     save_amount = float(list_amount[list_index])
@@ -48,25 +56,40 @@ for list_index in range(len(list_amount)):
     list_average.append(float(list_amount[list_index]) - save_amount)
     save_amount = float(list_amount[list_index])
    	   	
+# calculates the average change
 total_list_average = sum(list_average) / len(list_average)
 
+#-----Calculate Decrease in Profits--------
+#get the min amount to calculate the decrease in profit
 save_min_amount = float(min(list_amount))
-save_min_index = list_amount.index(save_min_amount)
-save_min_month_year = list_date[save_min_index]
 
+#gets the index of the min amount to use to retrieve the date from list_date  and previous amount
+save_min_index = list_amount.index(save_min_amount)
+
+#gets the date for the decrease in profit
+save_min_month_year = list_date[save_min_index]
+ 
+#gets the amount before the min to calculate the decrease in profit
 save_min_previous_amount = float(list_amount[save_min_index - 1])
 
 decrease_amount = save_min_amount - save_min_previous_amount
 
+#-----Calculate Increse in Profits--------
+#gets the max amount to calculate the increase in profit
 save_max_amount = float(max(list_amount))
+
+#gets the index of the max amount to use to retrieve the date from list_date  and previous amount
 save_max_index = list_amount.index(save_max_amount)
+
+#gets the date for the increase in profit
 save_max_month_year = list_date[save_max_index]
 
+#gets the amount before the max to calculate the increase in profit
 save_max_previous_amount = float(list_amount[save_max_index - 1])
 
 increase_amount = save_max_amount - save_max_previous_amount
 
-
+#Print Output
 print ('Financial Analysis')
 print ('------------------------')
 print (f'Total Months : {total_months}')
@@ -75,5 +98,18 @@ print (f'Average Change: ${round(total_list_average,2)}')
 print (f"Greatest Increase in amounts: {save_max_month_year} (${round(increase_amount)})")
 print (f"Greatest Decrease in amounts: {save_min_month_year} (${round(decrease_amount)})")
 
+#Write output in a text file
+with open(output_path, 'w') as text_file:
 
-    
+	text_file.write("Financial Analysis" + '\n')
+	text_file.write('------------------------' + '\n')
+	print(f"Total Months : {total_months}", file=text_file)
+	print(f'Total : ${round(total_profit_losses)}', file=text_file)
+	print(f'Average Change: ${round(total_list_average,2)}', file=text_file)
+	print(f"Greatest Increase in amounts: {save_max_month_year} (${round(increase_amount)})", file=text_file)
+	print(f"Greatest Decrease in amounts: {save_min_month_year} (${round(decrease_amount)})", file=text_file)
+
+
+
+
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,12 @@
-'''
-Name: Dinnara Hitt
-Python-Chalenge
-PyPoll 
-'''
+#Name: Dinnara Hitt
+#Python-Chalenge
+#PyPoll 
+
 import os
 import csv
 
-csvpath = os.path.join ('Resources', 'election_data.csv')
+csvpath = os.path.join ('Resources', 'PyPoll_Resources_election_data.csv')
+output_path = os.path.join ("Resources", "output_election_data.txt")
 
 total_votes = 0
 total_Khan = 0
@@ -20,7 +20,7 @@ list_total_votes = []
 
 winner_total = 0
 winner_index = 0
-winner_name = "Dinnara"
+winner_name = ""
 
 percent_Khan = 0.000
 percent_Correy = 0.000
@@ -28,18 +28,19 @@ percent_Li = 0.000
 percent_Tooley = 0.000
 
 with open(csvpath) as csvfile:
-	# CSV reader specifies delimiter and variable that holds contents
+	
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row first 
     csv_header = next(csvreader)
-    # Read each row of data after the header
+    
+    # for loop to read each row of data after the header 
+    # count votes for each candidate
     
     for row in csvreader:
-    	#list_date.append(row[0])
-    	#list_amount.append(float(row[1]))
+    	
     	total_votes += 1
-    	#total_profit_losses += int(row[1])
+    	
     	if row[2] == "Khan" :
     		total_Khan += 1 
     	elif row[2] == "Correy" :
@@ -51,22 +52,24 @@ with open(csvpath) as csvfile:
     	else:
     		total_none += 1
 
+#Calculates percentage of each candidate
 percent_Khan = float((total_Khan * 100) / total_votes)
 percent_Correy =  float((total_Correy * 100) / total_votes)
 percent_Li =  float((total_Li * 100) / total_votes)
 percent_Tooley =  float((total_Tooley * 100) / total_votes)
 
-
+#Insert candidates votes in list_total_votes to retrieve the winner
 list_total_votes.insert(0,total_Khan)
 list_total_votes.insert(1,total_Correy)
 list_total_votes.insert(2,total_Li)
 list_total_votes.insert(3,total_Tooley)
 
-#print (list_total_votes)
+#Find the max of votes and the name winner name(with the max index)
 winner_total = max(list_total_votes)
 winner_index = list_total_votes.index(winner_total)
 winner_name = list_candidates[winner_index]
 
+#Print Output
 print ('Election Results')
 print ('-----------------------------')
 print (f'Total votes: {total_votes}')
@@ -79,6 +82,20 @@ print ('-----------------------------')
 print (f"Winner: {winner_name}")
 print ('-----------------------------')
 
+#Write output in a text file
+with open(output_path, 'w') as text_file:
+
+    text_file.write('Election Results' + '\n')
+    text_file.write('-----------------------------' + '\n')
+    print (f'Total votes: {total_votes}', file=text_file)
+    print ('-----------------------------', file=text_file)
+    print (f'Khan: {round(percent_Khan,3)}% ({total_Khan})', file=text_file)
+    print (f'Correy: {round(percent_Correy,3)}% ({total_Correy})', file=text_file)
+    print (f'Li: {round(percent_Li,3)}% ({total_Li})', file=text_file)
+    print (f"O'Tooley: {round(percent_Tooley,3)}% ({total_Tooley})", file=text_file)
+    print ('-----------------------------', file=text_file)
+    print (f"Winner: {winner_name}", file=text_file)
+    print ('-----------------------------', file=text_file)
 
 
 
